068e32094776f9b74ea360feb3131f8e4b09d086
CharOConnell <charlotteoconnell@virginmedia.com>
2023-10-18 17:44:27 +0000

    added the form

--- src/Details.js
+++ src/Details.js
@@ -3,36 +3,38 @@ import jason from './jason0.jpg'
 function Details() {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 content-center'>
-            <div className='p-3'>
+            <div className='p-4'>
                 <img src={jason} alt="Jason" className="img-thumbnail img-fluid rounded"/>
             </div>
-            <div className='p-3'>
+            <div className='p-4'>
+                <hr className='flex border-black w-1/2 mx-auto md:border-0'></hr>
                 <p className="funky-font text-center py-3 mt-2 text-xl">DETAILS</p>
+                <hr className='flex border-black w-1/2 mx-auto mb-3'></hr>
                 <p className='pb-3'>
                     <strong className='funky-font py-3'>Timings:</strong><br></br>
                     Saturday 9th December from 6pm till midnight
                 </p>
-                <p className='pb-3'>
-                    <strong className='funky-font py-3'>Location:</strong><br></br>
-                    <a href='https://baranis.co.uk/' target='_blank'>Baranis</a>
-                    <p>
+                <div className='pb-3'>
+                    <p><strong className='funky-font pt-2'>Location:</strong></p><br></br>
+                    <a href='https://baranis.co.uk/' target='_blank' rel='noreferrer' className='btn border-2 rounded border-black p-1'>Baranis</a>
+                    <p className='pt-2'>
                         115 Chancery Lane <br></br>
                         London, WC2A 1PP
                     </p>
-                    <p>
+                    <p className='pb-3'>
                         Underground: <br></br>
                         Chancery Lane (6 min walk) <br></br>
                         Temple (7 min walk)
                     </p>
-                    <a href='https://goo.gl/maps/v5LiI' target='_blank'>Google Maps</a>
-                </p>
+                    <a href='https://goo.gl/maps/v5LiI' target='_blank' rel='noreferrer' className='btn border-2 rounded border-black p-1'>Google Maps</a>
+                </div>
                 <p className='pb-3'>
                     <strong className='funky-font py-3'>Dress Code:</strong><br></br>
                     Your best sparkly party gear!
                 </p>
                 <p className='pb-3'>
                     <strong className='funky-font py-3'>Catering:</strong><br></br>
-                    There will be food, wine and a <italic>cheeky cocktail</italic> for all
+                    There will be food, wine and a <strong>cheeky cocktail</strong> for all
                 </p>
                 <p className='pb-3'>
                     <strong className='funky-font py-3'>Gifts:</strong><br></br>
--- src/RsvpForm.js
+++ src/RsvpForm.js
@@ -5,12 +5,64 @@ function RsvpForm () {
     const onSubmit = data => console.log(data);
     
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("firstName", { required: true, maxLength: 20 })} />
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        <input type="number" {...register("age", { min: 18, max: 99 })} />
-        <input type="submit" />
-        </form>
+        // <form onSubmit={handleSubmit(onSubmit)}>
+        //     <input {...register("firstName", { required: true, maxLength: 20 })} />
+        //     <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+        //     <input type="number" {...register("age", { min: 18, max: 99 })} />
+        //     <input type="submit" />
+        // </form>
+        <div className='grid grid-cols-1 content-center mb-12'>
+            <hr className='flex border-black w-1/2 mx-auto'></hr>
+            <p className="funky-font text-center py-3 mt-2 text-xl">RSVP</p>
+            <hr className='flex border-black w-1/2 mx-auto mb-3'></hr>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="firstName" className="block mb-2 text-sm font-medium">Your (and your guest's) name:</label>
+                <div className="grid grid-cols-1">
+                    <div className="mb-2">
+                        <input className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                        focus:ring-yellow-300 focus:border-yellow-300 block w-5/6 p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300" 
+                        placeholder="Your name"
+                        {...register("firstName", { required: true, maxLength: 20 })} />
+                    </div>
+                    <div className="mb-3">
+                        <input className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                        focus:ring-yellow-300 focus:border-yellow-300 block w-5/6 p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300" 
+                        placeholder="Your guest's name"
+                        {...register("secondName", { required: false, maxLength: 20 })} />
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 my-4">
+                    <label htmlFor="attendance" className="block mb-2 text-sm font-medium">Will you be attending?</label>
+                    <div className="w-1/2 mx-auto">
+                        <div>
+                            <label htmlFor="yes">  
+                            <input className="justify-start" {...register("attendance")} type="radio" value="yes" id="attendanceYes"/> Yes</label>
+                        </div>
+                        <div>
+                            
+                            <label htmlFor="no">
+                            <input {...register("attendance")} type="radio" value="no" id="attendanceNo"/>  No</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 my-3">
+                    <div className="my-3">
+                        <label htmlFor="dietary" className="block mb-2 text-sm font-medium">Any dietary requirements?</label>
+                        <input className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                        focus:ring-yellow-300 focus:border-yellow-300 block w-5/6 p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300" 
+                        placeholder="Your dietary requirements"
+                        {...register("dietary", { required: false, maxLength: 20 })} />
+                    </div>
+                </div>
+
+                <input type="submit" className="baranis-yellow funky-font text-lg md:w-1/6 mx-auto hover:bg-yellow-500 
+                focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm sm:w-2/4 
+                px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"/>
+            </form>
+        </div>
     );
 }
 
