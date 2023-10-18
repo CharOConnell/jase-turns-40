6dee78b173d215d52edfeb86ed88d1c373330c78
CharOConnell <charlotteoconnell@virginmedia.com>
2023-10-18 18:26:04 +0000

    finalised and validated form

--- src/RsvpForm.js
+++ src/RsvpForm.js
@@ -1,16 +1,10 @@
 import { useForm } from "react-hook-form";
 
 function RsvpForm () {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     
     return (
-        // <form onSubmit={handleSubmit(onSubmit)}>
-        //     <input {...register("firstName", { required: true, maxLength: 20 })} />
-        //     <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        //     <input type="number" {...register("age", { min: 18, max: 99 })} />
-        //     <input type="submit" />
-        // </form>
         <div className='grid grid-cols-1 content-center mb-12'>
             <hr className='flex border-black w-1/2 mx-auto'></hr>
             <p className="funky-font text-center py-3 mt-2 text-xl">RSVP</p>
@@ -21,9 +15,11 @@ function RsvpForm () {
                     <div className="mb-2">
                         <input className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
                         focus:ring-yellow-300 focus:border-yellow-300 block w-5/6 p-2.5 dark:bg-gray-700 dark:border-gray-600 
-                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300" 
+                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300 
+                        ${errors.firstName ? 'border-red-600':''}" 
                         placeholder="Your name"
                         {...register("firstName", { required: true, maxLength: 20 })} />
+                        {errors.firstName && <p className="text-sm text-red-600">Please enter your name</p>}
                     </div>
                     <div className="mb-3">
                         <input className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
@@ -35,17 +31,13 @@ function RsvpForm () {
                 </div>
                 <div className="grid grid-cols-1 my-4">
                     <label htmlFor="attendance" className="block mb-2 text-sm font-medium">Will you be attending?</label>
-                    <div className="w-1/2 mx-auto">
-                        <div>
-                            <label htmlFor="yes">  
-                            <input className="justify-start" {...register("attendance")} type="radio" value="yes" id="attendanceYes"/> Yes</label>
-                        </div>
-                        <div>
-                            
-                            <label htmlFor="no">
-                            <input {...register("attendance")} type="radio" value="no" id="attendanceNo"/>  No</label>
-                        </div>
-                    </div>
+                    <select {...register("attendance", { required: true })} 
+                    className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                    focus:ring-yellow-300 focus:border-yellow-300 block w-5/6 p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                    dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
                 </div>
                 <div className="grid grid-cols-1 my-3">
                     <div className="my-3">
